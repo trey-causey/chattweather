e0cabd3a9a99ab96ad6796c52081ce842c413cad
trey-causey <trey.causey@outlook.com>
2020-06-01 19:49:53 -0400

    fixed more creds

--- resources/db/config.inc.php
+++ resources/db/config.inc.php
@@ -2,6 +2,6 @@
 
 define('DBUSER','sitejsvh_dbadmin,');
 define('DBPWD','!H!3E4vz1YHS');
-define('DBHOST','localhost');
-define('DBNAME','PerfectRun');
+define('DBHOST','127.0.0.1');
+define('DBNAME','sitejsvh_PerfectRun');
 define('DBPORT', '3306');
--- resources/db/dbtest.php
+++ resources/db/dbtest.php
@@ -1,11 +1,12 @@
 <?php
-
 include('config.inc.php');
 
 $link = mysqli_connect(DBHOST, DBUSER, DBPWD, DBNAME, DBPORT);
 
 if($link->connect_errno)
-{printf("that failed: %s, \n", $link->connect_error);}
+{
+    printf("That connection failed: %s, \n", $link->connect_error);
+}
 /*
 $sql = "SELECT * FROM `5kAdv`";
 
