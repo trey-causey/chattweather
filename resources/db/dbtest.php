6285e38247488871fbda4c8b8cf4ad21239b6873
trey-causey <trey.causey@outlook.com>
2020-06-01 16:14:40 -0400

    testing dbtest

--- resources/db/dbtest.php
+++ resources/db/dbtest.php
@@ -6,3 +6,24 @@ $link = mysqli_connect(DBHOST, DBUSER, DBPWD, DBNAME, DBPORT);
 
 if($link->connect_errno)
 {printf("that failed: %s, \n", $link->connect_error);}
+
+$sql = "SELECT * FROM '5kAdv'";
+
+$result = $link->query($sql);
+
+foreach ($result as $row)
+{
+    printf(
+        '<li>%s, %s, %s, %s, %s, %s, %s</li>li>',
+        htmlspecialchars($row['Week'], ENT_QUOTES),
+        htmlspecialchars($row['Monday'], ENT_QUOTES),
+        htmlspecialchars($row['Tuesday'], ENT_QUOTES),
+        htmlspecialchars($row['Wednesday'], ENT_QUOTES),
+        htmlspecialchars($row['Thursday'], ENT_QUOTES),
+        htmlspecialchars($row['Friday'], ENT_QUOTES),
+        htmlspecialchars($row['Saturday'], ENT_QUOTES),
+        htmlspecialchars($row['Sunday'], ENT_QUOTES)
+    );
+}
+
+$link->close();
