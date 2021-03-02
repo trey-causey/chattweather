fd939b1effbe99ecc06d19816499f3fb98348bec
trey-causey <trey.causey@outlook.com>
2021-03-02 11:21:23 -0500

    consolidated dev branch to work on Lando deployment

--- public/perfectrun.php
+++ public/perfectrun.php
@@ -35,8 +35,8 @@ for($i = 0; $i<7; $i++) {
     <meta name="generator" content="Jekyll v3.8.6">
     <title>Starter Template 路 Bootstrap</title>
 
-    <link rel="stylesheet" href="../resources/css/demo.css"/>
-    <link rel="stylesheet" href="../resources/css/theme3.css"/>
+    <link rel="stylesheet" href="css/demo.css"/>
+    <link rel="stylesheet" href="css/theme3.css"/>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/4.4/examples/starter-template/">
 
@@ -182,8 +182,8 @@ for($i = 0; $i<7; $i++) {
 
     </div>
 
-    <script type="text/javascript" src="../resources/js/caleandar.js"></script>
-    <script type="text/javascript" src="../resources/js/demo.js"></script>
+    <script type="text/javascript" src="js/caleandar.js"></script>
+    <script type="text/javascript" src="js/demo.js"></script>
 </main><!-- /.container -->
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script>window.jQuery || document.write('<script src="/docs/4.4/assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="/docs/4.4/dist/js/bootstrap.bundle.min.js" integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm" crossorigin="anonymous"></script></body>
